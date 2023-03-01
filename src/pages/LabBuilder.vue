1eaa073ef5f160b185a526615f7be74a2fa5bda4
koffer0-0 <68071028+Koffer0-0@users.noreply.github.com>
2023-03-01 14:24:56 +0600

    Update LabBuilder.vue

--- src/pages/LabBuilder.vue
+++ src/pages/LabBuilder.vue
@@ -1,22 +1,11 @@
 <template>
   <div>
+    here would be some message or what neponyanto or chto to obshee ili srazu
   </div>
 </template>
 
 <script>
-import DataTable from "@/components/UI/DataTable.vue";
-import ToolBar from "@/components/ToolBar.vue";
 
-// Data and labels
-const labels = [
-  { text: "ID", field: 'id' },
-  { text: "Name", field: 'name' },
-  { text: "Courses", field: 'date_created' },
-]
-const data = [
-  { id: 1, name: 'Foo', date_created: '01.01.2021' },
-  { id: 2, name: 'Bar', date_created: '01.01.2021' }
-];
 
 export default {
   name: "LabBuilder",
