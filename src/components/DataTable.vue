8d11e32e3c036488e8df14f109baecac0a17ed04
koffer0-0 <68071028+Koffer0-0@users.noreply.github.com>
2023-03-01 11:22:36 +0600

    add Datatable component
    Data component that allows you to maky dynamic columns and rows based on data

---
+++ src/components/DataTable.vue
@@ -0,0 +1,40 @@
+<template>
+  <table>
+    <thead>
+    <tr>
+      <td v-for="(label, labelIndex) in labels" :key="labelIndex">
+        {{ label.text }}
+      </td>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-for="(item, itemIndex) in data" :key="itemIndex">
+      <td v-for="(label, labelIndex) in labels" :key="labelIndex">
+        {{ item[label.field] }}
+      </td>
+    </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+
+// Data and labels
+export default {
+  name: "DataTable",
+  props: {
+    labels: {
+      type: Array,
+      required: true,
+    },
+    data: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/LabBuilder.vue
+++ src/pages/LabBuilder.vue
@@ -1,10 +1,33 @@
 <template>
-  <h4>lab builder</h4>
+  <div class="lab_builder">
+    <h4>lab builder</h4>
+    <DataTable :labels="labels" :data="data" />
+  </div>
 </template>
 
 <script>
+import DataTable from "@/components/DataTable.vue";
+
+// Data and labels
+const labels = [
+  { text: "ID", field: 'id' },
+  { text: "Name", field: 'name' },
+  { text: "Courses", field: 'date_created' },
+]
+const data = [
+  { id: 1, name: 'Foo', date_created: '01.01.2021' },
+  { id: 2, name: 'Bar', date_created: '01.01.2021' }
+];
+
 export default {
-  name: "LabBuilder"
+  name: "LabBuilder",
+  components: {DataTable},
+  data() {
+    return {
+      labels: labels,
+      data: data,
+    };
+  }
 }
 </script>
 
