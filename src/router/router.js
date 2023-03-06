9bf6ca7ddead4a65321bdb238e9eb2b048f53585
Miras_666 <52910217+mirastalbayev@users.noreply.github.com>
2023-03-06 14:20:27 +0600

    Убрал в компоненте routes в св-ве name дефис

--- src/router/router.js
+++ src/router/router.js
@@ -51,11 +51,11 @@ const router = new Router({
                         link: "/",
                     },
                     {
-                        name: "Lab-Builder",
+                        name: "Lab Builder",
                         link: "/lab-builder/stock-images",
                     },
                     {
-                        name: "Stock-Images",
+                        name: "Stock Images",
                     },
                 ],
             },
@@ -72,11 +72,11 @@ const router = new Router({
                         link: "/",
                     },
                     {
-                        name: "Lab-Builder",
+                        name: "Lab Builder",
                         link: "/lab-builder/lab-configuration",
                     },
                     {
-                        name: "Lab-Configuration",
+                        name: "Lab Configuration",
                     },
                 ],
             },
@@ -94,7 +94,7 @@ const router = new Router({
                         link: "/",
                     },
                     {
-                        name: "Course-Builder",
+                        name: "Course Builder",
                         link: "/course-builder/courses",
                     },
                     {
@@ -115,7 +115,7 @@ const router = new Router({
                         link: "/",
                     },
                     {
-                        name: "Course-Builder",
+                        name: "Course Builder",
                         link: "/course-builder/lectures",
                     },
                     {
@@ -136,7 +136,7 @@ const router = new Router({
                         link: "/",
                     },
                     {
-                        name: "Course-Builder",
+                        name: "Course Builder",
                         link: "/course-builder/labs",
                     },
                     {
@@ -158,7 +158,7 @@ const router = new Router({
                         link: "/",
                     },
                     {
-                        name: "User-Management",
+                        name: "User Management",
                         link: "/user-management/users",
                     },
                     {
@@ -179,7 +179,7 @@ const router = new Router({
                         link: "/",
                     },
                     {
-                        name: "User-Management",
+                        name: "User Management",
                         link: "/user-management/groups",
                     },
                     {
