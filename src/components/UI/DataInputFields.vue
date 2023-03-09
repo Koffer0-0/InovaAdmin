675eef04766db3f03b7f5319a1c01d90fd9c54ee
Miras_666 <52910217+mirastalbayev@users.noreply.github.com>
2023-03-09 15:32:52 +0600

    change

--- src/components/UI/DataInputFields.vue
+++ src/components/UI/DataInputFields.vue
@@ -1,59 +1,61 @@
 <template>
-  <div class="data_input">
-    <b-form-input
-        v-bind=""
-        v-model="formData.name"
-        :placeholder="name"
-        class="name_input"
-        @input="emit('update:name', $event.target.value)">
-    </b-form-input>
-    <b-form-textarea
-        v-model="formData.description"
-        :placeholder="description"
-        class="description"
-        @input="emit('update:description', $event.target.value)">
-    </b-form-textarea>
-  </div>
+    <div class="data_input">
+        <b-form-input
+            v-model="formData.name"
+            :placeholder="name"
+            class="name_input"
+            @input="emit('update:name', $event.target.value)"
+        >
+        </b-form-input>
+        <b-form-textarea
+            v-model="formData.description"
+            :placeholder="description"
+            class="description"
+            @input="emit('update:description', $event.target.value)"
+        >
+        </b-form-textarea>
+    </div>
 </template>
 
 <script>
 export default {
-  name: "DataInputFields",
-  props: {
-    name: {
-      type: String,
-      default: 'Name'
+    name: "DataInputFields",
+    props: {
+        name: {
+            type: String,
+            default: "Name",
+        },
+        description: {
+            type: String,
+            default: "Description",
+        },
+        formType: {
+            type: String,
+            required: true,
+            validator: (value) => ["lab", "lecture"].includes(value),
+        },
     },
-    description: {
-      type: String,
-      default: 'Description'
+    data() {
+        return {
+            formData: {
+                name: "",
+                description: "",
+            },
+        };
     },
-    formType: {
-      type: String,
-      required: true,
-      validator: value => ['lab', 'lecture'].includes(value),
-    },
-  },
-  data() {
-    return {
-      formData: {
-        name: '',
-        description: '',
-      },
-    };
-  },
-}
+};
 </script>
 
 <style scoped>
-  .name_input {
-    background-color: #E5E5E5;
-  }
-  .description {
-    background-color: #E5E5E5;
+.name_input {
+    background-color: #e5e5e5;
+}
+.description {
+    background-color: #e5e5e5;
     resize: none;
-  }
-  .data_input input, textarea {
+}
+.data_input input,
+textarea {
     margin-bottom: 20px;
-  }
+}
 </style>
